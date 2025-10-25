2f609b72ead3b285383a34972b0036ef971bc81c
Selin Uygun <selinjoe34@gmail.com>
2025-10-25 10:26:22 +0300

    integrated google sign-in

---
+++ lib/config/api_keys.dart
@@ -0,0 +1,11 @@
+/// Configuration file for API keys and credentials
+///
+/// WARNING: In production apps, sensitive keys should be:
+/// 1. Stored in environment variables
+/// 2. Added to .gitignore to prevent committing to version control
+/// 3. Retrieved from secure backend services when possible
+class ApiKeys {
+  // Google Sign-In Web Client ID
+  static const String googleWebClientId =
+      '354677682673-kfa5uk51cacb337hopnq4r7td9nu5suh.apps.googleusercontent.com';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'pages/login.dart';
 import 'pages/home.dart';
 import 'pages/search.dart';
 import 'pages/saved.dart';
@@ -22,7 +23,7 @@ class ClubsApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: const ClubsHome(),
+      home: const AuthWrapper(),
     );
   }
 }
